0ba65622f539d3d27cd2019f7d59f36e0ba23a56
YJHeo01 <93248202+YJHeo01@users.noreply.github.com>
2025-04-07 10:50:36 +0900

    Update solution.py

--- 2025-04-07/416. Partition Equal Subset Sum/solution.py
+++ 2025-04-07/416. Partition Equal Subset Sum/solution.py
@@ -2,11 +2,11 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         sum_value = sum(nums)
         if sum_value % 2 == 1: return False
+        sum_value //= 2
         dp = [False] * (sum_value+1)
         dp[0] = True
         for num in nums:
-            for i in range(sum_value,-1,-1):
+            for i in range(sum_value-num,-1,-1):
                 if dp[i] == True:
                     dp[i+num] = True
-        if dp[sum_value//2] == True: return True
-        return False
+        return dp[sum_value]
